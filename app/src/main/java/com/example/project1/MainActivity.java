1790bf472ce31b271c66298777b305d8d4f2b38b
Will Maahs <wmaahs@iastate.edu>
2023-10-04 00:04:30 -0500

    updated game UI

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -3,19 +3,24 @@ package com.example.project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public ImageView burg1;
-    public ImageView burg2;
-    public ImageView burg3;
-    public ImageView burg4;
-    public ImageView burg5;
-    public ImageView burg6;
-    public ImageView burg7;
-    public ImageView burg8;
-    public ImageView burg9;
+    private ImageButton burg1;
+    private ImageButton burg2;
+    private ImageButton burg3;
+    private ImageButton burg4;
+    private ImageButton burg5;
+    private ImageButton burg6;
+    private ImageButton burg7;
+    private ImageButton burg8;
+    private ImageButton burg9;
+
+    private int score = 0;
+    private boolean gameStarted = false;
 
 
     @Override
@@ -23,9 +28,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        burg1 = findViewById(R.id.window1);
+        burg2 = findViewById(R.id.window2);
+        burg3 = findViewById(R.id.window3);
+        burg4 = findViewById(R.id.window4);
+        burg5 = findViewById(R.id.window5);
+        burg6 = findViewById(R.id.window6);
+        burg7 = findViewById(R.id.window7);
+        burg8 = findViewById(R.id.window8);
+        burg9 = findViewById(R.id.window9);
+
+
+        burg1.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }));
+
 
 
 
 
     }
+
 }
