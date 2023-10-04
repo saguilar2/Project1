d4a8174c1bae7985d696c8a73bd10d1fc34f9c6f
wmaahs <wmaahs@iastate.edu>
2023-10-03 22:35:41 -0500

    updated game UI

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public ImageView burg1;
+    public ImageView burg2;
+    public ImageView burg3;
+    public ImageView burg4;
+    public ImageView burg5;
+    public ImageView burg6;
+    public ImageView burg7;
+    public ImageView burg8;
+    public ImageView burg9;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
     }
 }
